5da9f02aa24c8fa242395302c16415298089b9d8
Ro McKay Davies <50719703+120Davies@users.noreply.github.com>
2020-05-05 14:08:35 -0600

    Update lookup_table.py

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,15 +1,21 @@
+import sys
+sys.path.append('../../hashtable')
+from hashtable import HashTable
 import math
 import random
 
+slow_table = HashTable()
+
 def slowfun(x, y):
-    # TODO: Modify to produce the same results, but much faster
-    v = math.pow(x, y)
-    v = math.factorial(v)
-    v //= (x + y)
-    v %= 982451653
+    r = slow_table.get(str((x,y)))
+    if r is None:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
 
-    return v
-
+        return v
+    return r
 
 # Do not modify below this line!
 
